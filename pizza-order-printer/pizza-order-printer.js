e3901c66d52f928ca8a66440d2e6ee6742829856
ysabelbishop27 <ysabelbishop5150@gmail.com>
2026-06-25 14:59:11 -0500

    Update pizza-order-printer.js

--- pizza-order-printer/pizza-order-printer.js
+++ pizza-order-printer/pizza-order-printer.js
@@ -5,3 +5,28 @@ Make a Node.js app that prints pizza orders to the console.
 
 Check the README.md file for instructions.
 ------------------------- */
+const fs = require("fs/promises");
+
+const command = process.argv[2];
+const orderIndex = process.argv[3];
+
+async function pizzaOrderPrinter() {
+  const data = await fs.readFile("data.json", "utf-8");
+  const orders = JSON.parse(data);
+
+  if (command === "getAllOrders") {
+    orders.forEach((order) => {
+      console.log(order);
+    });
+  } else if (command === "getOneOrder") {
+    const order = orders[orderIndex];
+
+    if (order) {
+      console.log(order);
+    } else {
+      console.log("Order not found. Please enter a valid order number.");
+    }
+  }
+}
+
+pizzaOrderPrinter();
